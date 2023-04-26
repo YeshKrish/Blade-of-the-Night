6a396b9c48c26be04860b5ed5de2932841666925
YeshKrish <yeshwinkrishna@gmail.com>
2023-04-26 23:56:31 +0530

    PlayerInput Controller Script

--- Assets/_Scripts/PlayerInputComponent.cs
+++ Assets/_Scripts/PlayerInputComponent.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
-public class PlayerInputComponent : MonoBehaviour
+namespace BaldeOfNight
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerInputComponent : MonoBehaviour
     {
-        
+        public Vector2 MoveInput { get; private set; }
+        public Vector2 LastMoveInput { get; private set; }
+        public bool JumpInput { get; private set; }
+        public bool HasMoveInput { get; private set; }
+
+        public void OnMoveEvent(InputAction.CallbackContext context)
+        {
+            Vector2 moveInput = context.ReadValue<Vector2>();
+
+            bool hasMoveInput = moveInput.sqrMagnitude > 0.0f;
+            if(HasMoveInput && !hasMoveInput)
+            {
+                LastMoveInput = moveInput;
+            }
+
+            MoveInput = moveInput;
+            HasMoveInput = hasMoveInput;
+        }
+
+        public void OnJumpEvent(InputAction.CallbackContext context)
+        {
+            if (context.started || context.performed)
+            {
+                JumpInput = true;
+            }
+            else if (context.canceled)
+            {
+                JumpInput = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
